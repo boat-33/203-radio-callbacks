c84d0c73f750833ac0dca446e044ccdd3b989a85
Karl Boatright <karl_boatright@intuit.com>
2022-03-09 03:46:18 +0000

    modify options

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ import plotly.graph_objs as go
 
 ########### Define your variables ######
 
-myheading1='How to use callbacks'
+myheading1='Jazz Greats in History'
 tabtitle = 'jazz'
-list_of_options=['Art Blakey Drumming', 'correlation', 'git commit', 'scatterplot']
-list_of_images=['art_blakey.jpg', 'correlation.png', 'gitcommit.jpg', 'scatterplot.png', 'good_code.png']
+list_of_options=['Art Blakey Drumming', 'Hugh Masekela', 'Ornette Coleman', 'Don Cherry']
+list_of_images=['art_blakey.jpg', 'hugh_masekela.png', 'ornette_coleman.jpg', 'don_cherry.jpg', 'sonny_rollins.jpg']
 sourceurl = 'https://xkcd.com/'
 githublink = 'https://github.com/plotly-dash-apps/203-radio-callbacks'
 
